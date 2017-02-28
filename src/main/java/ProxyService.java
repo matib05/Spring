d53b02fc5cebee90464a0e4e0694369805d85835
Muhammad Ahmad <mahmad@uss.net>
2017-02-28 17:44:29 -0500

    re: Update ResponseEntity

--- src/main/java/ProxyService.java
+++ src/main/java/ProxyService.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -13,24 +14,23 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class ProxyService extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-		
+
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
-		
-		UriComponentsBuilder builder = request.();
+
+		UriComponentsBuilder builder = request.;
 		
 		HttpEntity<String> entity = new HttpEntity<>(headers);
-		
+
 		RestTemplate template = new RestTemplate();
-		HttpEntity<String> templateResponse = template.exchange(
-		        builder.build().encode().toUri(), 
-		        HttpMethod.GET, 
-		        entity, 
-		        String.class);
-		//research responesEntity
-		
+		ResponseEntity<String> templateResponse = template.exchange(
+				builder.build().encode().toUri(),
+				HttpMethod.GET,
+				entity,
+				String.class);
+
 	}
-	
+
 }
